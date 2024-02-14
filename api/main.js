bd583290e7b16446d6ea2f686b1178b4b389642b
Beau McCartney <mccartney.beausl@gmail.com>
2024-02-13 21:25:06 -0700

    frontend gets message from api

--- api/main.js
+++ api/main.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/')
+      .then(response => { setMessage(response.data)});
+  }, []);
 
   return (
     <>
-      <h1>Bear</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <h1>Message is: {message}</h1>
     </>
   )
 }
