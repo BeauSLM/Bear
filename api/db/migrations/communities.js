c7e68503f649f23abe1222a190bcc7cbf401ec51
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-04 21:19:09 -0700

    communities table

--- api/db/migrations/communities.js
+++ api/db/migrations/communities.js
@@ -1,10 +1,11 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('users', function(table) {
+    return knex.schema.createTable('communities', function(table) {
         table.increments('id').primary(); // GUID
         table.string('name').notNullable();
-        table.string('email').notNullable();
+        table.string('tagline').notNullable();
+        table.string('description').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.integer('owner_id').references('id').inTable('users');
     })
     // .createTable('logins', function(table) {
     //     table.integer('user_id').references('id').inTable('users');
@@ -14,5 +15,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('logins').dropTable('users');
+    return knex.schema.dropTable('communities');
 }
