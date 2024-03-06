97a07911b1deb14dffb7515b53fb2053e45c879c
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-05 19:03:37 -0700

    fix incorrect function call

--- api/db/migrations/reply.js
+++ api/db/migrations/reply.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('reply', function(table) {
         table.integer('thread_id').primary(); // GUID
-        table.incrementing('reply_id').primary(); // GUID
+        table.increments('reply_id').primary(); // GUID
         table.string('content', 2000).notNullable();
         table.integer('user_id').notNullable();
         table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
