cbaa904b068bd4f0b07cfc9d1f13069ebeeaebc7
AidanW86 <aidan.wong1@ucalgary.ca>
2024-03-05 20:52:16 -0700

    starting with community_mod, working on the rest

--- api/main.js
+++ api/main.js
@@ -18,6 +18,54 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
+// community_mod stuff
+app.get('/community_mod', async (req, res) => {
+  const communityMod = await db.select().from('community_mod');
+  res.json(communityMod);
+});
+
+app.get('/community_mod/:communityId/:userId', async (req, res) => {
+  const { communityId, userId } = req.params;
+  
+  try {
+    const communityMod = await db
+      .select()
+      .from('community_mod')
+      .where({ community_id: communityId, user_id: userId })
+      .first(); // Assuming only one result is expected
+    
+    if (communityMod) {
+      res.json(communityMod);
+    } else {
+      res.status(404).send('Community Mod not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching Community Mod');
+  }
+});
+
+app.post('/community_mod', async (req, res) => {
+  const newCommunityMod = req.body;
+  await db('community_mod').insert(newCommunityMod);
+  res.send('Community Mod created successfully');
+});
+
+app.delete('/community_mod/:communityId/:userId', async (req, res) => {
+  const { communityId, userId } = req.params;
+  try{
+    await db('community_mod').where({ community_id: communityId, user_id: userId }).del();
+
+    res.send('Community Mod deleted successfully');
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting community mod');
+  }
+});
+
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
