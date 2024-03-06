10bec8a359f931c927cd7a8b099dadc60f767e48
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-05 22:48:02 -0700

    fix: user table not users

--- api/db/migrations/community.js
+++ api/db/migrations/community.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
         table.string('tagline').notNullable();
         table.string('description').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.integer('owner_id').references('id').inTable('users');
+        table.integer('owner_id').references('id').inTable('user');
     })
 };
 
