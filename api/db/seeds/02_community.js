75c1227744464286097b7da036f0ce6dc9724145
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-20 10:32:50 -0600

    seeds: all communities owned by user 1

--- api/db/seeds/02_community.js
+++ api/db/seeds/02_community.js
@@ -4,18 +4,21 @@ exports.seed = function (knex, Promise) {
         .then(function () {
             return knex("community").insert([
                 {
+                    owner_id: 1,
                     name: "Cool Guys w/ Cars",
                     tagline: "Zoom zoom!",
                     description:
                         "Discuss your favorite cars with your silly car geeks!",
                 },
                 {
+                    owner_id: 1,
                     name: "BTD6 Comp. Chat",
                     tagline: "Tower defense is rad!",
                     description:
                         "A place to find, share, and engage with a community of like-minded individuals!",
                 },
                 {
+                    owner_id: 1,
                     name: "Pick-Up 52 Enthusiasts",
                     tagline: "How do you play?",
                     description:
