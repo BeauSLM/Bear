b91684e78a1ca9b5aca0f1f34747ebe737c73dac
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-19 18:36:02 -0600

    fix: non-broken api call to get users

--- api/db/populate_db.mjs
+++ api/db/populate_db.mjs
@@ -4,13 +4,18 @@ const ax = axios.create({
     baseURL: 'http://localhost:3001',
 })
 
-const hughjass = (await ax.get('user/1')).data
-const mikehawk = (await ax.get('user/2')).data
+const users = (await ax.get('user')).data
 
 const communities = (await ax.get('community')).data
 
-for (let c of communities) {
-    c.owner_id = hughjass.id
-    const c_json = JSON.stringify(c)
-    ax.put(`community/${c.id}`, c_json)
-}
+const hugh = users[0]
+const mike = users[1]
+
+console.log(hugh)
+console.log(mike)
+
+// for (let c of communities) {
+//     c.owner_id = hughjass.id
+//     const c_json = JSON.stringify(c)
+//     ax.put(`community/${c.id}`, c_json)
+// }
