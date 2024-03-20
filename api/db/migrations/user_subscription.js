7479b7f0d91874bad0434bbcf3cf0fe403e3e566
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-20 11:06:22 -0600

    seed up to community section working

--- api/db/migrations/user_subscription.js
+++ api/db/migrations/user_subscription.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('user_subscription', function(table) {
-        table.integer('user_id').primary().references('id').inTable('user'); // GUID
-        table.integer('community_id').primary().references('id').inTable('community'); // GUID
-        table.string('section').primary().references('section_name').inTable('community_section');
-    })
+    // return knex.schema.createTable('user_subscription', function(table) {
+    //     table.integer('user_id').primary().references('id').inTable('user'); // GUID
+    //     table.integer('community_id').primary().references('id').inTable('community'); // GUID
+    //     table.string('section').primary().references('section_name').inTable('community_section');
+    // })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('user_subscription');
+    // return knex.schema.dropTable('user_subscription');
 }
--- api/db/seeds/03_community_section.js
+++ api/db/seeds/03_community_section.js
@@ -5,7 +5,7 @@ exports.seed = function (knex, Promise) {
             return knex("community_section").insert([
                 {
                     community_id: 1,
-                    section_name: "engaging discussion",
+                    section_name: "section",
                     description: "like most things on the internet, reading this is totally a great use of your time...",
                 },
             ]);
