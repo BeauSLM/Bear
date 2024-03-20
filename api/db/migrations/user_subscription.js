f43334a662ac6de2230dd99eb2ca5fc799aec9bf
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-20 12:27:23 -0600

    bring back user subscription table

--- api/db/migrations/user_subscription.js
+++ api/db/migrations/user_subscription.js
@@ -1,12 +1,12 @@
 exports.up = function(knex, Promise) {
-    // return knex.schema.createTable('user_subscription', function(table) {
-    //     table.integer('user_id').primary().references('id').inTable('user'); // GUID
-    //     table.integer('community_id').primary().references('id').inTable('community'); // GUID
-    //     table.string('section').primary().references('section_name').inTable('community_section');
-        // table.integer('section_id').primary().references('section_id').inTable('community_section');
-    // })
+    return knex.schema.createTable('user_subscription', function(table) {
+        table.integer('user_id').primary().references('id').inTable('user'); // GUID
+        table.integer('community_id').primary().references('id').inTable('community'); // GUID
+        table.string('section').primary().references('section_name').inTable('community_section');
+        table.integer('section_id').primary().references('section_id').inTable('community_section');
+    })
 };
 
 exports.down = function(knex, Promise) {
-    // return knex.schema.dropTable('user_subscription');
+    return knex.schema.dropTable('user_subscription');
 }
