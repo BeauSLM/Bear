88890d93a18c485f3a1ab9636bd853d3523f7759
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-20 11:54:33 -0600

    bring back thread table

--- api/db/migrations/thread.js
+++ api/db/migrations/thread.js
@@ -1,16 +1,16 @@
 exports.up = function(knex, Promise) {
-    // return knex.schema.createTable('thread', function(table) {
-    //     table.integer('community_id').primary().references('id').inTable('community');
-    //     table.string('section', 50).primary().references('section_name').inTable('community_section');
-    //     table.increments('thread_id').primary();
-    //     table.string('title', 100).notNullable();
-    //     table.string('content', 2000).notNullable();
-    //     table.integer('user_id').notNullable().references('id').inTable('user');
-    //     table.integer('views').notNullable().defaultTo(0);
-    //     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-    // })
+    return knex.schema.createTable('thread', function(table) {
+        table.integer('community_id').primary().references('id').inTable('community');
+        table.string('section', 50).primary().references('section_name').inTable('community_section');
+        table.increments('thread_id').primary();
+        table.string('title', 100).notNullable();
+        table.string('content', 2000).notNullable();
+        table.integer('user_id').notNullable().references('id').inTable('user');
+        table.integer('views').notNullable().defaultTo(0);
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    })
 };
 
 exports.down = function(knex, Promise) {
-    // return knex.schema.dropTable('thread');
+    return knex.schema.dropTable('thread');
 }
