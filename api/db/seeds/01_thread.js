84734a6fc2422755ae017034a4bbdb0d92ea0974
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-19 21:31:37 -0600

    comment out seed for reply and thread
    initial seeding will be done using the script because they require
    non-null foreign keys
    
    eventually we'll factor out db interaction into a persistance layer that
    both the api and the db population scripts can call into

--- api/db/seeds/01_thread.js
+++ api/db/seeds/01_thread.js
@@ -2,19 +2,12 @@ exports.seed = function (knex, Promise) {
     return knex("thread")
         .del()
         .then(function () {
-            return knex("thread").insert([
-                {
-                    title: "I got a Miata",
-                    content: "The Miata would outclass any car or truck.",
-                    views: 17,
-                    created: knex.fn.now(),
-                },
-                {
-                    title: "I got a Ford F-150",
-                    content: "Don't think your Miata would do anything against my 2006 F-150.",
-                    views: 300,
-                    created: knex.fn.now(),
-                },
-            ]);
+            // return knex("thread").insert([
+            //     {
+            //         title: "I got a Ford F-150",
+            //         content: "Don't think your Miata would do anything against my 2006 F-150.",
+            //         views: 300,
+            //     },
+            // ]);
         });
 };
