c561dcbc3b989e2a2997d16de856ece402c60dd0
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-21 13:27:08 -0600

    community pages are keyed with a unique id integer

--- api/db/migrations/community_page.js
+++ api/db/migrations/community_page.js
@@ -1,7 +1,8 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('community_page', function(table) {
+        table.increments('page_id').primary();
         table.int('community_id').primary().references('id').inTable('community'); // GUID
-        table.string('page_name', 50).primary();
+        table.string('page_name', 50).unique();
         table.string('content').notNullable();
     })
 };
