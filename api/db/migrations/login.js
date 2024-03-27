c7a3174ba5a50ed2ee73acb423a37638383189a5
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-27 11:06:41 -0600

    login only needs one reference to user

--- api/db/migrations/login.js
+++ api/db/migrations/login.js
@@ -1,7 +1,6 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('login', function(table) {
         table.integer('user_id').references('id').inTable('user').primary();
-        table.string('username').references('name').inTable('user');
         table.string('passwordHash').notNullable();
     })
 };
