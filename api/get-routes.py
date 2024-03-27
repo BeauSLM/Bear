9e1e67acc179ed570b19f416200968a7feaaabf5
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-26 21:36:03 -0600

    update get-routes.py to support url on the next line
    prettier pass made one route have the url on the next line so lets do
    this now so it'll work in the future if more lines get formatted like
    this

--- api/get-routes.py
+++ api/get-routes.py
@@ -3,11 +3,18 @@ import re
 pattern = r"^app\.(get|put|post|delete)"
 
 with open('main.js', 'r') as file:
-    for line in file:
+    lines = file.readlines()
+    for ix, line in enumerate(lines):
         if not re.search(pattern, line): continue
         line = line.strip()
 
         req = line.split('app.')[1].split('(')[0]
-        url = line.split("'")[1]
+
+        if line.find("\"") == -1:
+            target = lines[ix + 1].strip()
+        else:
+            target = line
+
+        url = target.split("\"")[1]
 
         print(f"{req.upper():<7}{url}")
