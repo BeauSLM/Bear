a808f4541df80d7a0cfa40a07cec94634abc0859
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-27 15:08:01 -0600

    python script to get api routes called in frontend

---
+++ frontend/get-used-routes.py
@@ -0,0 +1,31 @@
+import subprocess
+import re
+
+api_calls = subprocess.check_output([
+    "ug",
+    "-d",
+    "recurse",
+    "--format=%O%~",
+    r"axios\.(get|put|post|delete)",
+]).decode().strip().split('\n')
+
+separators = ['\'', '`', '"']
+pattern = '|'.join(map(re.escape, separators))
+
+endpoints = {}
+for call in api_calls:
+    call = call.strip()
+    req = call.split('axios.')[1].split('(')[0]
+
+    url = re.split(pattern, call)[1]
+
+    endpoints.setdefault(req, set()).add(url)
+
+
+for req, urls in endpoints.items():
+    print(f"{req.upper()}:")
+    print("-" * 64)
+    print(f"{"\n".join(urls)}\n")
+
+
+
